e3916e2d156f4f5f9ae22256cae6b38a763de393
edvinasj <edvinasj@wix.com>
2019-01-09 20:13:10 +0200

    Refactor sign in form.

---
+++ app/components/SignInForm.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Button, Form, Grid, Header, Segment } from 'semantic-ui-react';
+
+function SignIn(props) {
+  const { handleChange, handleSubmit } = props;
+
+  return (
+    <div className="login-form">
+        <style>{`
+      body > div,
+      body > div > div,
+      body > div > div > div.login-form {
+        height: 100%;
+      }
+      `}</style>
+        <Grid
+          textAlign="center"
+          style={{ height: '100%' }}
+          verticalAlign="middle"
+        >
+          <Grid.Column style={{ maxWidth: 450 }}>
+            <Header as="h2" color="teal" textAlign="center">
+              PRESSCCOO
+            </Header>
+            <Form size="large">
+              <Segment stacked>
+                <Form.Input
+                  fluid
+                  icon="user"
+                  iconPosition="left"
+                  placeholder="E-mail address"
+                  name="username"
+                  onChange={handleChange}
+                />
+                <Form.Input
+                  fluid
+                  icon="lock"
+                  iconPosition="left"
+                  placeholder="Password"
+                  type="password"
+                  name="password"
+                  onChange={handleChange}
+                />
+                <Button
+                  color="teal"
+                  fluid
+                  size="large"
+                  onClick={handleSubmit}
+                >
+                  Login
+                </Button>
+              </Segment>
+            </Form>
+          </Grid.Column>
+        </Grid>
+      </div>
+  );
+}
+
+SignIn.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+  handleChange: PropTypes.func.isRequired,
+};
+
+export default SignIn;
--- app/containers/sign-in.js
+++ app/containers/sign-in.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Button, Form, Grid, Header, Segment } from 'semantic-ui-react';
 import { signIn } from '../actions/auth';
+import SignInForm from '../components/SignInForm'
 
 class SignInContainer extends Component {
   static contextTypes = {
@@ -44,58 +44,7 @@ class SignInContainer extends Component {
   }
 
   render() {
-    return (
-      <div className="login-form">
-        <style>{`
-      body > div,
-      body > div > div,
-      body > div > div > div.login-form {
-        height: 100%;
-      }
-    `}</style>
-        <Grid
-          textAlign="center"
-          style={{ height: '100%' }}
-          verticalAlign="middle"
-        >
-          <Grid.Column style={{ maxWidth: 450 }}>
-            <Header as="h2" color="teal" textAlign="center">
-              PRESSCCOO
-            </Header>
-            <Form size="large">
-              <Segment stacked>
-                <Form.Input
-                  fluid
-                  icon="user"
-                  iconPosition="left"
-                  placeholder="E-mail address"
-                  name="username"
-                  onChange={this.handleChange}
-                />
-                <Form.Input
-                  fluid
-                  icon="lock"
-                  iconPosition="left"
-                  placeholder="Password"
-                  type="password"
-                  name="password"
-                  onChange={this.handleChange}
-                />
-
-                <Button
-                  color="teal"
-                  fluid
-                  size="large"
-                  onClick={this.handleSubmit}
-                >
-                  Login
-                </Button>
-              </Segment>
-            </Form>
-          </Grid.Column>
-        </Grid>
-      </div>
-    );
+    return <SignInForm handleChange={this.handleChange} handleSubmit={this.handleSubmit}/>
   }
 }
 
