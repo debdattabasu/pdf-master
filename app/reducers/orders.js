4dfd984214c5d6afc51754df1005d7c280e1c0c8
edvinasj <edvinasj@wix.com>
2019-01-17 20:54:49 +0200

    fix order duplication bug

--- app/reducers/orders.js
+++ app/reducers/orders.js
@@ -1,15 +1,18 @@
-// @flow
 import { ADD_ORDER, FETCH_ORDERS, TOGGLE_ORDER } from '../actions/orders';
 import type { Action } from './types';
 
+const orderExists = (orders, orderId) => {
+  return orders.some((e) => e.id === orderId);
+}
+
 export default function orders(state = [], action: Action) {
   switch (action.type) {
-    // TODO add order duplication check
     case ADD_ORDER: {
-      const index = state.findIndex(el => el.id == action.id);
-      if(index == -1)
-        return [...state, action.event];
-      return state;
+      if (orderExists(state, action.id)) {
+        return state;
+      } else {
+        return [...state, action];
+      }
     }
     case FETCH_ORDERS: {
       return action.orders
