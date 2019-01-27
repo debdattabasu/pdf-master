8075bcdbc4bb8254b7a58e4e1b2ab69cdb637d21
edvinasj <edvinasj@wix.com>
2019-01-27 23:24:13 +0200

    Fire orders form to state.

--- app/components/GenerateTask.js
+++ app/components/GenerateTask.js
@@ -6,23 +6,38 @@ import {mapEmployeesToOptions, mapProductsToOptions} from '../utils/helpers';
 import {PRODUCTS} from '../constants/domain'
 
 export default class GenerateTask extends Component<Props> {
+  state ={
+    selectedProducts: [],
+    employee: undefined,
+    count: undefined,
+  }
+
   componentDidMount() {
     const {fetchEmployees} = this.props;
     fetchEmployees();
   }
 
+  handleProductChange = (e, { value }) => this.setState({ selectedProducts: value })
+
+  handleEmployeeChange = (e, { value }) => this.setState({ employee: value })
+
+  onCountChange = (e, { value }) => this.setState({ count: value })
+
   renderForm = () => {
     const {employees} = this.props;
+    const {selectedProducts, employee} = this.state;
     const employeeOptions = mapEmployeesToOptions(employees);
     const productOptions = mapProductsToOptions(PRODUCTS);
+    const previewEnabled = employee && selectedProducts.length > 0;
 
     return (
       <Form>
         <Form.Group widths='equal'>
-          <Form.Select fluid label='Employee' options={employeeOptions} placeholder='Pick One'/>
-          <Form.Select fluid label='Product Types' options={productOptions} placeholder='Products' multiple selection/>
+          <Form.Select onChange={this.handleProductChange} fluid label='Product Types' options={productOptions} placeholder='Products' multiple selection required/>
+          <Form.Select onChange={this.handleEmployeeChange} fluid label='Employee' options={employeeOptions} placeholder='Pick One' required width={5}/>
+          <Form.Input onChange={this.onCountChange} fluid label='Order count' placeholder='Number of orders' type="number" width={5}/>
         </Form.Group>
-        <Form.Button>Preview Tasks</Form.Button>
+        <Form.Button positive disabled={!previewEnabled}>Preview Tasks</Form.Button>
       </Form>
     );
   }
