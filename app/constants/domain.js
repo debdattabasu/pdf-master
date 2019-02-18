f8add6683cfcf834902e6e3c8278450a94305c72
edvinasj <edvinasj@wix.com>
2019-02-18 23:57:30 +0200

    update domain.

--- app/constants/domain.js
+++ app/constants/domain.js
@@ -1,4 +1,4 @@
-export const PRODUCTS = ['T-shirt/Shirt', 'T-Shirt', 'T­shirt', 'Shirt', 'child', 'Hoodie', 'Crewneck', 'Bags', 'Case', 'Mug', 'Pillow',  'marškinėliai', 'capuche', 'Protection', 'film'];
+export const PRODUCTS = ['T-shirt/Shirt', 'T-Shirt', 'T­shirt', 'Shirt', 'child', 'Hoodie', 'Crewneck', 'Bags', 'Case', 'Mug', 'Pillow',  'marškinėliai', 'capuche', 'Protection', 'Film'];
 
 export const LANGUAGES = {
   ENGLISH: 'eng',
